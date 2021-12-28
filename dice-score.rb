1b7198d4b2dede35f4ebd825a80680d5641504b7
Amit Singh Bisht <amit.bi@browserstack.com>
2021-12-28 19:42:45 +0530

    comments added

--- dice-score.rb
+++ dice-score.rb
@@ -5,7 +5,6 @@ class DiceScore
         element_to_be_deleted_from_index = []
         sum = 0                                                         # initialize sum variable to be equal to 0
         counter = 0                                                     # initialize counter variable to be equal to 0, we will be using counter variable to keep a track of the index
-        scoring_dice = 0                                                # to keep a track of scoring dices, If all of the thrown dice are scoring, then the player may roll all 5 dice in the next roll.
         for i in 0...dice.size
             score = 0
             if i == counter                                             # initially i and counter both are 0
@@ -15,24 +14,20 @@ class DiceScore
                     element_to_be_deleted_from_index << counter+1       # add counter + 1 to the array (element_to_be_deleted_from_index)
                     element_to_be_deleted_from_index << counter+2       # add counter + 2 to the array (element_to_be_deleted_from_index)
                     counter += 3                                        # also counter is incremented by 3
-                    scoring_dice += 3                                   # and scoring_dice gets incremented by 3
                 elsif dice[counter] == dice[counter+1] && dice[counter] == dice[counter+2]
                     score = 100 * dice[counter]                         # if there are three consecutive same numbers then the player gets 100 times the number points
                     element_to_be_deleted_from_index << counter         # add counter to the array (element_to_be_deleted_from_index)
                     element_to_be_deleted_from_index << counter+1       # add counter + 1 to the array (element_to_be_deleted_from_index)
                     element_to_be_deleted_from_index << counter+2       # add counter + 2 to the array (element_to_be_deleted_from_index)
                     counter += 3                                        # also counter is incremented by 3
-                    scoring_dice += 3                                   # and scoring_dice gets incremented by 3
                 elsif dice[counter] == 1   
                     score = 100                                         # if the number on dice appears to be 1, give a score of 100
                     element_to_be_deleted_from_index << counter         # add counter to the array (element_to_be_deleted_from_index)
                     counter += 1                                        # also counter is incremented by 1
-                    scoring_dice += 1                                   # and scoring_dice gets incremented by 1
                 elsif dice[counter] == 5  
                     score = 50                                          # if the number on dice appears to be 5, give a score of 50
                     element_to_be_deleted_from_index << counter         # add counter to the array (element_to_be_deleted_from_index)
                     counter += 1                                        # also counter is incremented by 1
-                    scoring_dice += 1                                   # and scoring_dice gets incremented by 1
                 else
                     counter += 1                                        # also counter is incremented by 1
                 end
@@ -43,6 +38,6 @@ class DiceScore
         element_to_be_deleted_from_index.each do |element_at_index|
             dice.delete_at(element_at_index)                            # delete the element from respective index
         end
-        [sum, scoring_dice, dice]                                       # return sum, scoring_dice and dice(new) as an array
+        [sum, dice]                                                     # return sum and dice(new) as an array
     end
 end
--- greedy.rb
+++ greedy.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # -*- ruby -*-
 
-require "./dice-roll"
+require "./dice-roll"                                                                                                                                               # we will require the class files for our project
 require "./dice-score"
 
 puts ""
@@ -10,75 +10,74 @@ puts "                           GREED GAME                           "
 puts "================================================================"
 
 print "\nEnter number of players: "
-number_of_players = gets.chomp.to_i
+number_of_players = gets.chomp.to_i                                                                                                                                 # gets number of users playing our game
 
-# Create an array to keep track of the score of players
-score_of_players = Hash.new()
+score_of_players = Hash.new()                                                                                                                                       # Create an array to keep track of the score of players
 number_of_players.times do |i|
-    score_of_players[i] = 0
+    score_of_players[i] = 0                                                                                                                                         # initialize hash with number of players as key and value as 0
 end
 
-is_score_greater_than_3000 = false
-is_round_completed = false
-current_round = 1
-rounds_to_reach_3000 = -1
+is_score_greater_than_3000 = false                                                                                                                                  # flag to check if any player has reached the benchmark of 3000 points
+is_round_completed = false                                                                                                                                          # if anyone is reached, then the next round will be the last round
+current_round = 1                                                                                                                                                   # current_round will keep a track of rounds and display it on our console screen
+rounds_to_reach_3000 = -1                                                                                                                                           # initialize second last round with -1
 
-dice_roll_object = DiceRoll.new
-dice_score_object = DiceScore.new
+dice_roll_object = DiceRoll.new                                                                                                                                     # object dice_roll_object is created for class DiceRoll
+dice_score_object = DiceScore.new                                                                                                                                   # object dice_score_object is created for class DiceScore
 
-until is_round_completed
+until is_round_completed                                                                                                                                            # game will continue till the time any player reaches a score of 3000 and set the flag is_round_completed = true
     puts ""
     puts "----------------------------------------------------------------"
     puts "                              Turn #{current_round}"
     puts "----------------------------------------------------------------"
-    current_player = 0
-    number_of_players.times do
-        score_accumulated_for_this_round = 0
-        dice_count = 5
-        go_for_next_roll = "y"
-        all_5_dice_rolled_in_first_attempt = true
-        while go_for_next_roll == "y"
-            dice_values = dice_roll_object.roll_a_dice(dice_count)
+    current_player = 0                                                                                                                                              # begin with first player whose indexing is 0
+    number_of_players.times do                                                                                                                                      # iterate as many number of times as the players playing
+        score_accumulated_for_this_round = 0                                                                                                                        # score_accumulated_for_this_round will keep a track of score for that particular round/turn
+        dice_count = 5                                                                                                                                              # initially all the 5 dices are to be rolled
+        go_for_next_roll = "y"                                                                                                                                      # go_for_next_roll will ask the player if they want to roll further or stop their turn. Initially kept as "y (yes)"
+        all_5_dice_rolled_in_first_attempt = true                                                                                                                   # all_5_dice_rolled_in_first_attempt will help us to know whether or not all the 5 dices scored point
+        while go_for_next_roll == "y"                                                                                                                               # condition to check if player wants to continue further or not
+            dice_values = dice_roll_object.roll_a_dice(dice_count)                                                                                                  # calls roll_a_dice method that returns an array
             print "\nPlayer #{current_player + 1} rolls : #{dice_values} \n"
-            score, scoring_dice, dice_values = dice_score_object.score_on_roll(dice_values)
-            print "  Score in this roll : #{score}\n"
-            if score == 0
+            score, dice_values = dice_score_object.score_on_roll(dice_values)                                                                                       # pass the same array as a parameter to score_on_roll method that returns 2 value - score generated from that dice, and new dice which consists of elements that didn't score
+            print "  Score in this roll : #{score}\n"                               
+            if score == 0                                                                                                                                           # if no score is generated, then player loses its turn and also the score generated from that turn also gets lost
                 print "  Score in this round : "
                 print (score_accumulated_for_this_round > 0) ? 
                     "0 (Ah ! High Risk High Reward sometimes may backfire. You lost all your points for this round)\n" : "0\n"
-                score_accumulated_for_this_round = 0
-                go_for_next_roll = "n"
+                score_accumulated_for_this_round = 0                                                                                                                # set score_accumulated_for_this_round = 0, as player just lost
+                go_for_next_roll = "n"                                                                                                                              # player should not roll again for that turn
             else
-                dice_count = dice_values.size
-                score_accumulated_for_this_round += score
+                dice_count = dice_values.size                                                                                                                       # check the size of new array to find how many dice have scored points
+                score_accumulated_for_this_round += score                                                                                                           # add the score to score_accumulated_for_this_round variable
                 print "  Score in this round : #{score_accumulated_for_this_round}"
-                print (score_of_players[current_player] == 0 and score_accumulated_for_this_round < 300) ? 
-                    " (Minimum score: 300. You still need #{300 - score_accumulated_for_this_round} score to add this turn score to your total score)\n" : "\n"
+                print (score_of_players[current_player] == 0 and score_accumulated_for_this_round < 300) ?                                                          # Before a player is allowed to accumulate points, they must get atleast 300 points in a single turn. Once they have achieved 300 points
+                    " (Minimum score: 300. You still need #{300 - score_accumulated_for_this_round} score to add this turn score to your total score)\n" : "\n"     # in a single turn, the points earned in that turn and each following turn will be counted toward their total score
                 print "  Non-Scoring Dice : #{dice_values}\n"
-                if dice_count > 0
-                    all_5_dice_rolled_in_first_attempt = false
+                if dice_count > 0                                                                                                                                   # check if dice_count is > 0, means not all dice have scored points
+                    all_5_dice_rolled_in_first_attempt = false                                                                                                      # hence set the flag all_5_dice_rolled_in_first_attempt to false
                     print "  Do you want to roll the non-scoring #{dice_count} dice? (y/n): " 
-                    go_for_next_roll = (gets.chomp.to_s.downcase)[0]
-                elsif dice_count == 0 and all_5_dice_rolled_in_first_attempt
+                    go_for_next_roll = (gets.chomp.to_s.downcase)[0]                                                                                                # gets player input if they further want to roll the dice, if "y" follow same procedure again or else go to next player
+                elsif dice_count == 0 and all_5_dice_rolled_in_first_attempt                                                                                        # if all dices have scored points 
                     print "  Tremendous ! All In. You have scored points on all 5 dice. Extra roll is provided to you. "
-                    dice_count = 5
+                    dice_count = 5                                                                                                                                  # then player should get a chance to roll all 5 dice again as a reward 
                     go_for_next_roll = "y"
                 else
-                    go_for_next_roll = "n"
+                    go_for_next_roll = "n"                                                                                                                          # if player enters "n", pass the game to the next player
                 end
             end 
         end
-        (score_of_players[current_player] == 0 and score_accumulated_for_this_round < 300) ? 
+        (score_of_players[current_player] == 0 and score_accumulated_for_this_round < 300) ?                                                                        # if a player gets 300 or more points then add it to their previous score or else no score is awarded
             0 : score_of_players[current_player] += score_accumulated_for_this_round
-        current_player += 1
+        current_player += 1                                                                                                                                         # next player plays the game
     end
     puts "\n\nScore after Turn #{current_round} : "
     score_of_players.each do |key, value|
         puts "  Player #{key+1} : #{value}"
     end
     puts "\n"
-    is_round_completed = true if is_score_greater_than_3000 && current_round > rounds_to_reach_3000
-    is_score_greater_than_3000, rounds_to_reach_3000 = true, current_round if score_of_players.values.max >= 3000
+    is_round_completed = true if is_score_greater_than_3000 && current_round > rounds_to_reach_3000                                                                 # set the flag is_round_completed to true if the score is greater than 3000 and current_round (last round)> rounds_to_reach_3000(which we had set in previous step => last round - 1)
+    is_score_greater_than_3000, rounds_to_reach_3000 = true, current_round if score_of_players.values.max >= 3000                                                   # see if any player has reached the benchmark score of 3000 or not and rounds_to_reach_3000 as current_round (last round - 1)
     puts "Player #{score_of_players.key(score_of_players.values.max) + 1} has reached 3000 (or more points). The next round is the final round and the player with the highest score wins !!! \n" if is_score_greater_than_3000 && !is_round_completed
     puts "Hurray !!! Player #{score_of_players.key(score_of_players.values.max) + 1} has won !!! \n\n" if is_score_greater_than_3000 && is_round_completed
     current_round += 1
