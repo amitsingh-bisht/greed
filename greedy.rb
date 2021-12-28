10b5ce1a19b67207cc98bb02162fca67076d8a0f
Amit Singh Bisht <amit.bi@browserstack.com>
2021-12-28 11:02:36 +0530

    dice integrated successfully

--- greedy.rb
+++ greedy.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
-$LOAD_PATH << File.dirname(__FILE__)
-require 'Dice'
+# -*- ruby -*-
+
+require "./dice"
 
 puts "================================"
 puts "           GREED GAME           " 
@@ -12,5 +13,5 @@ number_of_players = gets.chomp.to_i
 # Create an array to keep track of the score of players
 score_of_players = Array.new(number_of_players, 0)
 
-dice = Dice.new()
-print dice.roll_a_dice()
+b = Dice.new
+puts b.roll_a_dice()
